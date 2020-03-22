77b957c4df821f2dd60dab1cb49dd4e0d8758b45
Tatyana Silchenko <taniasilchenko212@gmail.com>
2020-03-22 18:03:32 +0300

    feat:add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,40 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error();
+    var result = [];
+
+    for (var i = 0; i < arr.length; i++) {
+
+        if (arr[i] == undefined) {
+            result = result.slice(0, result.length - 1);
+            continue;
+        }
+
+        if (arr[i] === '--double-next') {
+            if (i !== (arr.length - 1)) {
+                result.push(arr[i + 1]);
+            }
+            continue;
+        }
+
+        if (arr[i] === '--double-prev') {
+            if (i !== 0) {
+                result.push(arr[i - 1]);
+            }
+            continue;
+        }
+
+        if (arr[i] === '--discard-next') {
+            i++;
+            continue;
+        }
+
+        if (arr[i] === '--discard-prev') {
+            result = result.slice(0, result.length - 1)
+            continue;
+        }
+
+        result.push(arr[i]);
+    }
+    return result;
+}
